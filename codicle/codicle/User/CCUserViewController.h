8f6b05462dea006592c75f86a4b1d1e4e7ec4bd3
SHOHE <ohtani.s@nagisa-inc.jp>
2015-03-04 12:51:47 +0900

    CCUserViewController +

---
+++ codicle/codicle/User/CCUserViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCUserViewController.h
+//  codicle
+//
+//  Created by SHOHE on 3/4/15.
+//  Copyright (c) 2015 OhtaniShohe. All rights reserved.
+//
+
+#import "CCBaseViewController.h"
+
+@interface CCUserViewController : CCBaseViewController
+
+@end
---
+++ codicle/codicle/User/CCUserViewController.m
@@ -0,0 +1,37 @@
+//
+//  CCUserViewController.m
+//  codicle
+//
+//  Created by SHOHE on 3/4/15.
+//  Copyright (c) 2015 OhtaniShohe. All rights reserved.
+//
+
+#import "CCUserViewController.h"
+
+@interface CCUserViewController ()
+
+@end
+
+@implementation CCUserViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
