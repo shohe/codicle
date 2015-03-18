1ed55e442a8c1b990ea10dbe9e3d53cdf4cccdfc
SHOHE <ohtani.s@nagisa-inc.jp>
2015-03-18 17:24:14 +0900

    create postViewController.

---
+++ codicle/codicle/Post/CCPostViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCPostViewController.h
+//  codicle
+//
+//  Created by SHOHE on 3/18/15.
+//  Copyright (c) 2015 OhtaniShohe. All rights reserved.
+//
+
+#import "CCBaseViewController.h"
+
+@interface CCPostViewController : CCBaseViewController
+
+@end
---
+++ codicle/codicle/Post/CCPostViewController.m
@@ -0,0 +1,37 @@
+//
+//  CCPostViewController.m
+//  codicle
+//
+//  Created by SHOHE on 3/18/15.
+//  Copyright (c) 2015 OhtaniShohe. All rights reserved.
+//
+
+#import "CCPostViewController.h"
+
+@interface CCPostViewController ()
+
+@end
+
+@implementation CCPostViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
