c373c70c6a6dcda33899af8f8200486911e78386
SHOHE <ohtani.s@nagisa-inc.jp>
2015-03-18 13:26:54 +0900

    create class for search for channel

---
+++ codicle/codicle/Cell/CCChannelSearchCell.h
@@ -0,0 +1,13 @@
+//
+//  CCChannelSearchCell.h
+//  codicle
+//
+//  Created by SHOHE on 3/18/15.
+//  Copyright (c) 2015 OhtaniShohe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCChannelSearchCell : UITableViewCell
+
+@end
---
+++ codicle/codicle/Cell/CCChannelSearchCell.m
@@ -0,0 +1,23 @@
+//
+//  CCChannelSearchCell.m
+//  codicle
+//
+//  Created by SHOHE on 3/18/15.
+//  Copyright (c) 2015 OhtaniShohe. All rights reserved.
+//
+
+#import "CCChannelSearchCell.h"
+
+@implementation CCChannelSearchCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
