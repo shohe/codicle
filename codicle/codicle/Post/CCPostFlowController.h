73bc4210d561f75ac2f2fec9069aad5c109bf29d
SHOHE <ohtani.s@nagisa-inc.jp>
2015-03-19 02:16:48 +0900

    create post flow controller

---
+++ codicle/codicle/Post/CCPostFlowController.h
@@ -0,0 +1,13 @@
+//
+//  CCPostFlowController.h
+//  codicle
+//
+//  Created by SHOHE on 3/19/15.
+//  Copyright (c) 2015 OhtaniShohe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCPostFlowController : UICollectionViewController
+
+@end
