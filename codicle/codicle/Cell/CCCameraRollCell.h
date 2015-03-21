7773cebc00a3fac7773ed60adeba0a9bafbb9756
SHOHE <ohtani.s@nagisa-inc.jp>
2015-03-21 17:50:24 +0900

    CollectionViewControllerCell for CameraRoll +

---
+++ codicle/codicle/Cell/CCCameraRollCell.h
@@ -0,0 +1,13 @@
+//
+//  CCCameraRollCell.h
+//  codicle
+//
+//  Created by SHOHE on 3/21/15.
+//  Copyright (c) 2015 OhtaniShohe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCCameraRollCell : UICollectionViewCell
+
+@end
---
+++ codicle/codicle/Cell/CCCameraRollCell.m
@@ -0,0 +1,13 @@
+//
+//  CCCameraRollCell.m
+//  codicle
+//
+//  Created by SHOHE on 3/21/15.
+//  Copyright (c) 2015 OhtaniShohe. All rights reserved.
+//
+
+#import "CCCameraRollCell.h"
+
+@implementation CCCameraRollCell
+
+@end
