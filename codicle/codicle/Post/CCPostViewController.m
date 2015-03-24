9f702a36a34c04d2e785f86fd4ce8545b26a155a
SHOHE <ohtani.s@nagisa-inc.jp>
2015-03-24 10:05:43 +0900

    edit get CameraRoll data process order.

--- codicle/codicle/Post/CCPostViewController.m
+++ codicle/codicle/Post/CCPostViewController.m
@@ -17,15 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [CCCORE loadCameraRollListWithCompletion:^(NSError *error, NSArray *mary) {
-        if (!error && [mary count] > 0) {
-            NSMutableArray *array = [NSMutableArray array];
-            for (NSDictionary *data in [mary reverseObjectEnumerator]) {
-                [array addObject:data];
-            }
-            [CCCORE setCameraRollData:array];
-        }
-    }];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
