7b74e677192aef764b38c015373b772242c5f7d4
SHOHE <ohtani.s@nagisa-inc.jp>
2015-03-24 20:23:36 +0900

    add new NavigationController

---
+++ codicle/codicle/Post/CCPostNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  CCPostNavigationController.h
+//  codicle
+//
+//  Created by SHOHE on 3/24/15.
+//  Copyright (c) 2015 OhtaniShohe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCPostNavigationController : UINavigationController
+
+@end
---
+++ codicle/codicle/Post/CCPostNavigationController.m
@@ -0,0 +1,37 @@
+//
+//  CCPostNavigationController.m
+//  codicle
+//
+//  Created by SHOHE on 3/24/15.
+//  Copyright (c) 2015 OhtaniShohe. All rights reserved.
+//
+
+#import "CCPostNavigationController.h"
+
+@interface CCPostNavigationController ()
+
+@end
+
+@implementation CCPostNavigationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
