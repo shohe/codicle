7405507e9ba7d0a698f03af11c8bbdfef7ad7728
SHOHE <ohtani.s@nagisa-inc.jp>
2015-03-25 04:38:15 +0900

    add new library

---
+++ codicle/codicle/Library/UIPlaceHolderTextView/UIPlaceHolderTextView.h
@@ -0,0 +1,13 @@
+//
+//  UIPlaceHolderTextView.h
+//  codicle
+//
+//  Created by SHOHE on 3/25/15.
+//  Copyright (c) 2015 OhtaniShohe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIPlaceHolderTextView : UITextView
+
+@end
---
+++ codicle/codicle/Library/UIPlaceHolderTextView/UIPlaceHolderTextView.m
@@ -0,0 +1,21 @@
+//
+//  UIPlaceHolderTextView.m
+//  codicle
+//
+//  Created by SHOHE on 3/25/15.
+//  Copyright (c) 2015 OhtaniShohe. All rights reserved.
+//
+
+#import "UIPlaceHolderTextView.h"
+
+@implementation UIPlaceHolderTextView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
